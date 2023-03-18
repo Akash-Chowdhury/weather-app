29543db60da73c422de4dce0130af3e775974373
Akash <chowdhuryakash950@gmail.com>
2023-03-18 20:39:10 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import CurrentLocation from "./currentLocation";
+import "./App.css";
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className="container">
+        <CurrentLocation />
+      </div>
+      <div className="footer-info">
+         Developed by{" "}
+          Akash Chowdhury
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/apiKeys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  key: "677c59ae50bf98f97baecb3a65ebefd3",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
